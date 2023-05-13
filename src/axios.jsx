69af83c71aa0996a8f39679c2dd66f21ed388097
Saurabh kumar <109866847+Saurabhparshar@users.noreply.github.com>
2023-05-13 10:36:42 +0530

    Delete src directory

--- src/axios.jsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const baseURL = "https://api.spoonacular.com";
-
-export default axios.create({
-  baseURL,
-  params: { apiKey: '3c4e80cf68d84f95aa06f8bbe120f8a9' },
-});
